6232075e0e62a84f88f70243a23184ba7583f8b5
Willians Patiño <ppwj@yahoo.com>
2024-04-22 16:01:03 -0400

    Specify app environtment

---
+++ app_env.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# ruta del clone git
+CLONEDAPP_DIR="$HOME/optimize-ux-console"       ##  DO NOT CHANGE
+
+# Components
+helpers=$CLONEDAPP_DIR/helpers.sh
+
+# target dir  
+SYSTEMDIRECTORY="/opt"
+
+# script principal
+#MAIN_="$SYSTEMDIRECTORY/$CLONEDREPOSITORY/main.sh"
